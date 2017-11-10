c3adc141f93351cf4b085d1534a1a210a82b8cdf
Mariusz <info@mbachurski.com>
2017-11-10 09:49:56 -0800

    added interval update, and leading '0'

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -17,30 +17,31 @@ class Clock extends Component {
         this.getTimeUntil(this.props.deadline);
     }
 
+    componentDidMount() {
+        setInterval( () => this.getTimeUntil(this.props.deadline), 1000);
+    }
+
+    leading0(num) {
+        return num < 10 ? '0' + num : num;
+    }
+
     getTimeUntil(deadline) {
          const time = Date.parse(deadline) - Date.parse(new Date());
-         console.log('time', time);
          const seconds = Math.floor((time/1000) % 60); 
          const minutes = Math.floor((time/1000/60) % 60);
          const hours = Math.floor(time/(1000*60*60) % 24);
          const days = Math.floor(time/(1000*60*60*24));
-         console.log('days', days, 'hours', hours, 'minutes', minutes, 'seconds', seconds );
 
-         this.setState({ 
-            days: days,
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds
-         });
+         this.setState({ days, hours, minutes, seconds });
     }
 
     render() {
         return(
             <div>
-                <div className="Clock-days">{ this.state.days } Days</div>
-                <div className="Clock-hours">{ this.state.hours } Hours</div>
-                <div className="Clock-minutes">{ this.state.minutes } Minutes</div>
-                <div className="Clock-seconds" >{ this.state.seconds } Seconds</div>
+                <div className="Clock-days">{ this.leading0(this.state.days) } Days</div>
+                <div className="Clock-hours">{ this.leading0(this.state.hours) } Hours</div>
+                <div className="Clock-minutes">{ this.leading0(this.state.minutes) } Minutes</div>
+                <div className="Clock-seconds" >{ this.leading0(this.state.seconds) } Seconds</div>
             </div>
         )
     }
