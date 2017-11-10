afe6fca75c49e38786f94acdf0e4a4eb834019ae
Mariusz <info@mbachurski.com>
2017-11-09 22:51:41 -0800

    Added structure for countdown

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
 
-    render(
-        <div> Countdown Champ, App Component</div>
-    )
+    render() {
+        return (
+            <div className="App">
+                <div className="App-title">Countdown to December 25, 2017</div>
+                <div>
+                    <div className="Clock-days">14 Days</div>
+                    <div className="Clock-hours">30 Hours</div>
+                    <div className="Clock-minutes">15 Minutes</div>
+                    <div className="Clock-seconds" >20 Seconds</div>
+                </div>
+                <div>
+                    <input placeholder="new date" />
+                    <button>Submit</button>
+                </div>
+            </div>
+        )
+       
+    }
 }
+
+export default App;
